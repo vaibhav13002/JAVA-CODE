9efba04a2d4c476f6f69b080246b2159815bf278
vaibhav13002 <vaibhavmishra.bs@gmail.com>
2023-07-18 20:37:22 +0530

    code added

---
+++ My Java/Array2DList.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Array2DList {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the no:");
+        ArrayList<ArrayList> list=new ArrayList<>(2);
+
+        //Initiliazing
+        for(int i=0; i<5;i++){
+            list.add(new ArrayList<>(5));
+        }
+
+    //Adding element
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+                list.get(i).add(sc.nextInt());  //pehle array list pakdengey i pr nd usme fill krengey
+            }
+        }
+        System.out.println(list);
+    }
+}
