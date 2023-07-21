7b97a88b7d527a00301f0d849ab3a4ca66a5cd21
vaibhav13002 <vaibhavmishra.bs@gmail.com>
2023-07-21 12:51:20 +0530

    code added

---
+++ My Java/CounTArray.java
@@ -0,0 +1,12 @@
+public class CountNumberofNumbers {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5};
+
+    }
+    static int count1(int[] arr){
+        int dig=0;
+        for(int i=0;i<arr.length;i++){
+            
+        }
+    }
+}
