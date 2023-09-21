a2157141be89b15c75b6a314898cdc2bfbae4a5c
vaibhav13002 <vaibhavmishra.bs@gmail.com>
2023-09-21 23:04:44 +0530

    code added

--- DSA JAVA/FindElementRecur.java
+++ DSA JAVA/FindElementRecur.java
@@ -1,10 +1,20 @@
+import java.util.ArrayList;
+
 public class FindElementRecur {
     public static void main(String[] args) {
-        int[] arr={1,2,14,3,25};
+        int[] arr={1,2,14,14,3,25};
         int target=14;
-        System.out.println(find(arr, target, 0));
-        System.out.println(PrintIndex(arr, target, 0));
+        // System.out.println(find(arr, target, 0));
+        // System.out.println(PrintIndex(arr, target, 0));
+
+        // PrintIndex1(arr, target, 0);
+        // System.out.println(list);
+
+
+        
+        System.out.println(PrintIndex2(arr, target, 0, new ArrayList<Integer>()));
     }
+    //Normal code for to check if there or not
     public static boolean find(int[] arr, int target, int index){
         if(target==arr.length-1){
             return true;
@@ -36,4 +46,33 @@ public class FindElementRecur {
         return PrintIndex(arr, target, index+1);
         }
 }
+
+//for multiple same target we can use arraylist
+static ArrayList<Integer> list= new ArrayList<>();
+public static void PrintIndex1(int[] arr, int target, int index){
+        if(target==arr.length-1){
+            list.add(index);
+        }
+        if(index==arr.length){ //base case
+            return;
+        }
+        if(target==arr[index]){
+            list.add(index);
+        }
+        PrintIndex1(arr, target, index+1);
+}
+//Defining Arraylist in the function so that it is available for that function
+ static ArrayList<Integer> PrintIndex2(int[] arr, int target, int index, ArrayList<Integer> list1){
+        if(target==arr.length-1){
+            list1.add(index);
+        }
+        if(index==arr.length){ //base case
+            return list1;
+        }
+        if(target==arr[index]){
+            list1.add(index);
+        }
+        return PrintIndex2(arr, target, index+1, list1);
+}
+
 }
