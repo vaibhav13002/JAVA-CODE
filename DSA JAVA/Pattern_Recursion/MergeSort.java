a98dbc2ad49a803e136114415df4aad93b25a43c
vaibhav13002 <vaibhavmishra.bs@gmail.com>
2023-09-23 17:00:03 +0530

    code added

---
+++ DSA JAVA/Pattern_Recursion/MergeSort.java
@@ -0,0 +1,49 @@
+package Pattern_Recursion;
+import java.util.* ;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] arr = {2,4,1,6,5,3};
+        arr=mergesort(arr);  //have to update an array so fun calling will be like this
+        System.out.println(Arrays.toString(arr));
+        // System.out.println(Arrays.toString(mergesort(arr)));
+    }
+    static int[] mergesort(int[] arr){
+        if(arr.length == 1){
+            return arr;
+        }
+        int mid=arr.length/2;
+
+        int[] left=mergesort(Arrays.copyOfRange(arr, 0, mid));
+        int[] right=mergesort(Arrays.copyOfRange(arr, mid, arr.length));
+
+        return merge(left, right);
+    }
+    private static int[] merge(int[] left, int[] right) {
+        int[] mix= new int[left.length + right.length];
+        int i=0; //left
+        int j=0; //right
+        int k=0;
+        while(i<left.length && j<right.length){ //a tab tk valid h jab tk dono me same elemnent h agr ek jayda bada h tho usko alag se last me add krna hoga
+            if(left[i]<right[j]){
+                mix[k]=left[i];
+                i++;
+            }else{
+                mix[k]=right[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<left.length){
+            mix[k]=left[i];
+            i++;
+            k++;
+        }
+            while(j<right.length){
+            mix[k]=right[j];
+            j++;
+            k++;
+        }
+        return mix;
+    }
+}
