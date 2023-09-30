267f18b7225917bf25f8c49beb4fd49d7545d052
vaibhav13002 <vaibhavmishra.bs@gmail.com>
2023-09-30 19:57:55 +0530

    code added

---
+++ DSA JAVA/Pattern_Recursion/QuickSort.java
@@ -0,0 +1,37 @@
+package Pattern_Recursion;
+
+import java.util.Arrays;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr={5,4,3,2,1};
+        sort(arr, 0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void sort(int[] nums, int low, int hig){
+        if(low > hig){
+            return;
+        }
+        int s=low;
+        int e=hig;
+        int mid= s=(e-s)/2;
+        int pivot= nums[mid];
+
+        while (s<=e) {
+//also a reason why if it is already sorted it will not swap
+            while (nums[s] < pivot) {
+                s++;
+            }
+            while (nums[e] > pivot) {
+                e--;
+            }
+            if (s <= e) {
+                int temp=nums[s];
+                nums[s]=nums[e];
+                nums[e]=temp;
+            }
+        }
+        sort(nums, low, e);
+        sort(nums, s, hig);
+    }
+}
