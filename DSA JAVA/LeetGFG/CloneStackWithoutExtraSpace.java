326a3a9e0665ccd10800ab18629cd2b9fdcd93a5
VaishaliMishra111 <mishravaishali111@gmail.com>
2024-02-10 22:27:21 +0530

    code added

---
+++ DSA JAVA/LeetGFG/CloneStackWithoutExtraSpace.java
@@ -0,0 +1,29 @@
+package LeetGFG;
+//https://www.geeksforgeeks.org/problems/clone-a-stack-without-usinig-extra-space/0
+import java.util.Stack;
+
+public class CloneStackWithoutExtraSpace {
+    public static void main(String[] args) {
+        Stack<Integer> originalStack = new Stack<>();
+        originalStack.push(1);
+        originalStack.push(2);
+        originalStack.push(3);
+
+        Stack<Integer> clonedStack = new Stack<>();
+        clonestack(originalStack, clonedStack);
+
+        // Print the cloned stack
+        while (!clonedStack.isEmpty()) {
+            System.out.print(clonedStack.pop() + " ");
+        }
+    }
+    static void clonestack(Stack<Integer> st, Stack<Integer> cloned) {
+       if(st.size()==0){
+           return;
+       }
+       int t=st.peek();
+       st.pop();
+       clonestack(st,cloned);   // draw recursion tree call
+       cloned.push(t);
+    }
+}
