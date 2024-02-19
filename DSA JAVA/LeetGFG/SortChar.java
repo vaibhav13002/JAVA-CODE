201afe35bf1e4d3ee5c4548c5f0b78ba0bd4dc6a
VaishaliMishra111 <vaibhavmishra.bs@gmail.com>
2024-02-19 22:48:22 +0530

    code added

---
+++ DSA JAVA/LeetGFG/SortChar.java
@@ -0,0 +1,18 @@
+package LeetGFG;
+
+import java.util.Arrays;
+
+public class SortChar {
+    public static void main(String[] args) {
+        String s = "aabcdabbcf";
+
+        // converting into char array
+        char[] arr = s.toCharArray();
+        Arrays.sort(arr);
+        System.out.println(arr);
+
+        //Converting char array bcak to string
+        String st= new String(arr);
+        System.out.println(st);
+    }
+}
