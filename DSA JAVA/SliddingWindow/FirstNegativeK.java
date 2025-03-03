810df8ad2cbd7b4e4c022678bf5e158508b7dabf
vaibhav13002 <vaibhavmishra.bs@gmail.com>
2025-03-03 23:16:27 +0530

    code added

--- DSA JAVA/SliddingWindow/FirstNegativeK.java
+++ DSA JAVA/SliddingWindow/FirstNegativeK.java
@@ -8,55 +8,58 @@ import java.util.Queue;
 
 public class FirstNegativeK {
     public static void main(String[] args) {
-        int[] arr = {-8, 2, 3, -6, 10};
+        int[] arr = { -8, 2, 3, -6, 10 };
         int k = 2;
         System.out.println(FirstNegativeInteger(arr, k));
         System.out.println(FirstNegativeIntegerOptimized(arr, k));
     }
-        static List<Integer> FirstNegativeInteger(int arr[], int k) {
+
+    static List<Integer> FirstNegativeInteger(int arr[], int k) {
         ArrayList<Integer> list = new ArrayList<>();
-        
-        for(int i=0; i<arr.length-k+1; i++){
+
+        for (int i = 0; i < arr.length - k + 1; i++) {
             boolean found = false;
-            for(int j=i; j<i+k; j++){
-                if(arr[j] < 0){
+            for (int j = i; j < i + k; j++) {
+                if (arr[j] < 0) {
                     list.add(arr[j]);
                     found = true;
                     break;
                 }
             }
-            if(!found){
+            if (!found) {
                 list.add(0);
             }
         }
         return list;
     }
-        static List<Integer> FirstNegativeIntegerOptimized(int arr[], int k) {
+
+    static List<Integer> FirstNegativeIntegerOptimized(int arr[], int k) {
         Queue<Integer> q = new LinkedList<>();
         int index = 0;
-        while(index < k ){
-            if(arr[index] < 0){
-                q.offer(arr[index]);
+        while (index < k) {    // For the optimzed solution in sliding window hume first window ko khud se create krne hoga if we want to use concept prev element remove and next element add.
+            if (arr[index] < 0) {   
+                q.offer(arr[index]);     // Agr first window me -ve element mil rha h tho usko queue me add krdo 
             }
             index++;
         }
         ArrayList<Integer> ans = new ArrayList<>();
-        if(q.isEmpty()){
+        if (q.isEmpty()) {    // Now start filling answer, if there was no -ve element in the queue then add Zero
+            ans.add(0);
+        } else {
+            ans.add(q.peek());   // agar -ve hai tho top -ve ko add krdo // abhi tak humne -ve element ko remove nhi kiya h
+        }
+        for (int i = 1; i < arr.length - k + 1; i++) {  // Now the 1 element, first window ka humne krdiya h it means loop 1 se chalu hoga and we can use arr[i-1]
+            if (arr[i - 1] < 0) {  // agar 1st element in the queue is -ve jo ki humne ans me already add krdiya h ussey nikal do
+                q.poll();   
+            }
+            if (arr[i + k - 1] < 0) {  // Incrasing the window, next element of the window, if it is -ve add it into the queue
+                q.offer(arr[i + k - 1]);
+            }
+// Above 2 steps me humne window ko slide kiya h, and at the same time queue ko khali
+            if (q.isEmpty()) {  // now for the next window again check ki khali h mtlb uss window me sab +ve element tha
                 ans.add(0);
-            }else{
-                ans.add(q.peek());
-            }
-        for(int i=1; i<arr.length-k+1; i++){
-            if(arr[i-1] < 0){
-                q.poll();
-            }
-            if(arr[i+k-1] < 0){
-                q.offer(arr[i+k-1]);
-            }
-                    if(q.isEmpty()){
-                ans.add(0);
-            }else{
-                ans.add(q.peek());
+            } else {
+                ans.add(q.peek());  // agr empty nhi h tho mtlb -ve element he h , coz bas vahi hum queue me add kr rhe h
             }
         }
         return ans;
---
+++ DSA JAVA/SliddingWindow/SlidingWindowMax239.java
@@ -0,0 +1,39 @@
+package SliddingWindow;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+
+public class SlidingWindowMax239 {
+    public static void main(String[] args) {
+        int[] arr = {1,3,-1,-3,5,3,6,7};
+        int k=3;
+        System.out.println(Arrays.toString(SlidingMax(arr, k)));
+    }
+    public static int[] SlidingMax(int[] arr , int k){
+        int n= arr.length;
+        int[] ans = new int[n+k-1];
+        if(n == 0){
+            return ans;
+        }
+        Deque<Integer> q = new ArrayDeque<>();
+        int index =0;
+        while(index < k){
+            while(!q.isEmpty() && arr[q.peekLast()] <= arr[index]){
+                q.pollLast();
+            }
+            q.offerLast(index);
+            index++;
+        }
+        ans[0] = arr[q.peekFirst()];
+        for(int i=1; i<n-k+1; i++){
+            if(!q.isEmpty() && q.peekFirst() <= (i-1)){
+                q.pollFirst();
+            }
+            q.offerLast(i+k-1);
+            ans[i] = arr[q.peekFirst()];
+        }
+        return ans;
+    }
+}
