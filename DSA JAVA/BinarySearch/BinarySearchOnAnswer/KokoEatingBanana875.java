9a1e7176547dc8f8c7fabe71a44d83cad8958e34
vaibhav13002 <vaibhavmishra.bs@gmail.com>
2025-03-21 22:12:43 +0530

    code added

--- DSA JAVA/BinarySearch/BinarySearchOnAnswer/KokoEatingBanana875.java
+++ DSA JAVA/BinarySearch/BinarySearchOnAnswer/KokoEatingBanana875.java
@@ -2,33 +2,34 @@ package BinarySearch.BinarySearchOnAnswer;
 
 public class KokoEatingBanana875 {
     public static void main(String[] args) {
-        int[] piles = {3,6,7,11};
+        int[] piles = { 3, 6, 7, 11 };
         int hours = 8;
-        System.out.println(Bruteforce(piles, hours));
+        System.out.println(minEatingSpeed(piles, hours));
     }
+
     public static int Bruteforce(int[] piles, int hours) {
         int maxPile = 0;
-        
+
         // Find the maximum pile (worst case, eating 1 pile per hour)
         for (int pile : piles) {
             maxPile = Math.max(maxPile, pile);
         }
-        
+
         // Start from speed 1 and go up to maxPile
         for (int time = 1; time <= maxPile; time++) {
             int total = 0;
-            
+
             // Calculate total hours needed for this speed
             for (int pile : piles) {
                 total += (int) Math.ceil((double) pile / time);
             }
-    
+
             // If the total hours required is within the given limit, return time
             if (total <= hours) {
                 return time;
             }
         }
-        
+
         return maxPile; // Worst case, if no other speed works
 
         // piles =
@@ -36,4 +37,41 @@ public class KokoEatingBanana875 {
         // h =
         // 823855818
     }
+
+    //////////////Optimized//////////
+
+    public static int minEatingSpeed(int[] piles, int h) {
+        int maxElement = maxElement(piles);
+        int low = 1;
+        int high = maxElement;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int timeTaken = function(piles, mid);
+            if (timeTaken <= h) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    public static int function(int[] arr, int pile) {
+        int total = 0;
+        for (int i = 0; i < arr.length; i++) {
+            total += Math.ceil((double) (arr[i]) / (double) (pile));
+        }
+        return total;
+    }
+
+    public static int maxElement(int[] piles) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < piles.length; i++) {
+            if (piles[i] > max) {
+                max = piles[i];
+            }
+        }
+        return max;
+    }
+
 }
